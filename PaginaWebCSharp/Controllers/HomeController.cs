09b63867af12d82fe5325c83a799bf69fb275a06
byrond27 <byrond27@gmail.com>
2018-01-17 13:02:13 -0500

    Initial commit

---
+++ PaginaWebCSharp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sercop-Admin
+ * Date: 17/01/2018
+ * Time: 12:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Web.Mvc;
+
+namespace PaginaWebCSharp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		
+		public ActionResult Contact()
+		{
+			return View();
+		}
+	}
+}
---
+++ PaginaWebCSharp/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sercop-Admin
+ * Date: 17/01/2018
+ * Time: 12:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PaginaWebCSharp
+{
+	public class MvcApplication : HttpApplication
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.Ignore("{resource}.axd/{*pathInfo}");
+			
+			routes.MapRoute(
+				"Default",
+				"{controller}/{action}/{id}",
+				new {
+					controller = "Home",
+					action = "Index",
+					id = UrlParameter.Optional
+				});
+		}
+		
+		protected void Application_Start()
+		{
+			RegisterRoutes(RouteTable.Routes);
+		}
+	}
+}
